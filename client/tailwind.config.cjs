b8c1743694ea4ed1bab618b431a0e78704c37328
spencerandtheteagues <spencerandtheteagues@gmail.com>
2025-08-31 02:52:25 -0500

    fix(build): simplify tailwind content paths

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,10 +4,8 @@ const path = require("path");
 module.exports = {
   darkMode: ["class"],
   content: [
-    path.join(__dirname, "index.html"),
-    path.join(__dirname, "src/**/*.{js,ts,jsx,tsx}"),
-    path.join(__dirname, "src/components/**/*.{js,ts,jsx,tsx}"),
-    path.join(__dirname, "src/pages/**/*.{js,ts,jsx,tsx}"),
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
